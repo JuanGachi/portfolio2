96432a830c3b8ae55c57c4f754fe6421444e48fd
= <=>
2024-03-15 18:25:46 +0100

    15/03/2024

--- Proyectos personales/cursodjango/webplayground/pages/views.py
+++ Proyectos personales/cursodjango/webplayground/pages/views.py
@@ -1,10 +1,24 @@
 
+from django.http import HttpRequest
+from django.http.response import HttpResponse as HttpResponse
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.admin.views.decorators import staff_member_required
+from django.utils.decorators import method_decorator
 from django.urls import reverse, reverse_lazy
+from django.shortcuts import redirect
 from .models import Page
 from .forms import PageForm
+
+class StaffRequiredMixin(object):
+    """
+    Este mixin requerira que el usuario sea miembro del staff
+    """
+    @method_decorator(staff_member_required)
+    def dispatch(self, request, *args, **kwargs):
+        
+        return super(StaffRequiredMixin, self).dispatch(self, request, *args, **kwargs)
 
 # Create your views here.
 class PageListView(ListView):
@@ -13,11 +27,14 @@ class PageListView(ListView):
 class PageDetailView(DetailView):
     model = Page
 
+@method_decorator(staff_member_required, name='dispatch')
 class PageCreate(CreateView):
     model = Page
     form_class = PageForm
     success_url = reverse_lazy('pages:pages')
 
+    
+@method_decorator(staff_member_required, name='dispatch')
 class PageUpdate(UpdateView):
     model = Page
     form_class = PageForm
@@ -25,6 +42,8 @@ class PageUpdate(UpdateView):
     
     def get_success_url(self):
         return reverse_lazy('pages:update', args=[self.object.id]) + '?ok'
+    
+@method_decorator(staff_member_required, name='dispatch')
 class PageDelete(DeleteView):
     model = Page
     success_url = reverse_lazy('pages:pages')
--- Proyectos personales/cursodjango/webplayground/webplayground/settings.py
+++ Proyectos personales/cursodjango/webplayground/webplayground/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'core',
+    'messenger',
     'pages.apps.PagesConfig',
+    'profiles',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,19 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Auth redirects
+#LOGIN_REDIRECT_URL = 'pages:pages'
+LOGOUT_REDIRECT_URL = 'home'
+
+# Emails
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+    EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
+else:
+    # Aquí hay que configurar un email real para producción
+    pass
+
+# Media Files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
